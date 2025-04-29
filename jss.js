c4906039fb79a5874e59670f6dce66701f38ac96
Kate <martynovakata98908@gmail.com>
2025-04-29 23:19:54 +0300

    Merge remote-tracking branch 'origin/Kate-feature' into Kate-feature

---
+++ jss.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu')
+menu = '.menu'
+menu.addEventListener('.click', function(){
+    window.location.href = ('http://localhost:63342/untitledTest/menu.html?_ijt=p58r5bqntpuk941itlfphbjir3&_ij_reload=RELOAD_ON_SAVE')
+
+})
